2c00a83cc0a92e41e4545a84a73d46303c214da2
Steven Phan <ptviet@live.com>
2020-08-17 19:15:50 +1000

    Update Env vars

--- frontend/epic-collabs-frontend/deploy.sh
+++ frontend/epic-collabs-frontend/deploy.sh
@@ -6,10 +6,10 @@ echo "Deploying epic-collabs to STAGE: $STAGE"
 
 yarn --silent vercel --prod \
 -b STAGE="$STAGE" \
--b ORG_ID="$NOW_ORG_ID" \
--b PROJECT_ID="$NOW_PROJECT_ID" \
+-b ORG_ID="$VERCEL_ORG_ID" \
+-b PROJECT_ID="$VERCEL_PROJECT_ID" \
 -b COMMIT_HASH="$BUILDKITE_COMMIT" \
 -b BUILD_NUMBER="$BUILDKITE_BUILD_NUMBER" \
---token "$NOW_TOKEN"
+--token "$VERCEL_TOKEN"
 
 echo "Deployment complete"
