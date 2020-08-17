411271caf3ee87ff826bd4a11c92d5a437dc24d4
Steven Phan <ptviet@live.com>
2020-08-17 18:52:18 +1000

    Add NOW_ prefix to env

--- frontend/epic-collabs-frontend/deploy.sh
+++ frontend/epic-collabs-frontend/deploy.sh
@@ -2,15 +2,14 @@
 
 set -euo pipefail
 
-echo "Deploying epic-collabs..."
-echo "STAGE: $STAGE"
+echo "Deploying epic-collabs to STAGE: $STAGE"
 
 yarn --silent vercel --prod \
 -b STAGE="$STAGE" \
--b ORG_ID="$ORG_ID" \
--b PROJECT_ID="$PROJECT_ID" \
+-b ORG_ID="$NOW_ORG_ID" \
+-b PROJECT_ID="$NOW_PROJECT_ID" \
 -b COMMIT_HASH="$BUILDKITE_COMMIT" \
 -b BUILD_NUMBER="$BUILDKITE_BUILD_NUMBER" \
---token "$VERCEL_TOKEN"
+--token "$NOW_TOKEN"
 
 echo "Deployment complete"
