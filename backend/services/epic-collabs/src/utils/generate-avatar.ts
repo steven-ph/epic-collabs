5d802bb286c255113bfb8b75d3aaaca4a11c190f
Steven Phan <ptviet@live.com>
2020-08-25 00:49:08 +1000

    Use uuid

--- backend/services/epic-collabs/src/utils/generate-avatar.ts
+++ backend/services/epic-collabs/src/utils/generate-avatar.ts
@@ -1,9 +1,5 @@
-import md5 from 'md5';
+import { v4 as uuidv4 } from 'uuid';
 
-const generateAvatar = () => {
-  const random = Date.now().toString().replace(/\s+/g, '');
-
-  return `https://gravatar.com/avatar/${md5(random)}?d=identicon`;
-};
+const generateAvatar = () => `https://gravatar.com/avatar/${uuidv4()}?d=identicon`;
 
 export { generateAvatar };
