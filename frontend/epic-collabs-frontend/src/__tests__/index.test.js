70f49870f5906aec6442842394cede8bbc3b9b77
Steven Phan <ptviet@live.com>
2020-08-30 21:30:27 +1000

    Fix test

--- frontend/epic-collabs-frontend/src/__tests__/index.test.js
+++ frontend/epic-collabs-frontend/src/__tests__/index.test.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { render, screen } from 'testUtils';
 import Index from '../../pages';
 
+jest.mock('../hooks/use-project.js', () => ({
+  useGetProjects: jest.fn().mockResolvedValue({
+    projects: []
+  })
+}));
+
 describe('Index Page', () => {
   it('should show the heading', () => {
     render(<Index />);
