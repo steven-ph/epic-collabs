80ec2d7306c947c36f62a3d7c89179667aeb08f7
Steven Phan <ptviet@live.com>
2020-09-01 13:46:06 +1000

    Refactor imgs

--- frontend/epic-collabs-frontend/src/components/projects/index.js
+++ frontend/epic-collabs-frontend/src/components/projects/index.js
@@ -1,34 +1,40 @@
 import React from 'react';
 import { values } from 'lodash';
-import { Row, Col } from 'antd';
 import styled from 'styled-components';
-import { breakpoints, colours, easing } from 'styles';
+import { easing } from 'styles';
 import { useGetProjects } from 'hooks/use-project';
-import { Box, Flexbox } from 'components/common';
+import { Flexbox } from 'components/common';
+
+const Project = ({ project }) => {
+  return (
+    <Item>
+      <img src={project.image} alt={project.name} />
+    </Item>
+  );
+};
 
 const Projects = () => {
   const { projects } = useGetProjects();
-  const data = [
-    ...values(projects),
-    ...values(projects),
-    ...values(projects),
-    ...values(projects),
-    ...values(projects),
-    ...values(projects),
-    ...values(projects),
-    ...values(projects),
-    ...values(projects),
-    ...values(projects),
-    ...values(projects),
-    ...values(projects)
+  const data = values(projects);
+  const items = [
+    ...data,
+    ...data,
+    ...data,
+    ...data,
+    ...data,
+    ...data,
+    ...data,
+    ...data,
+    ...data,
+    ...data,
+    ...data,
+    ...data
   ];
 
   return (
     <Container>
-      {data.map((project, index) => (
-        <Item key={index}>
-          <img src={project.image} alt={project.name} />
-        </Item>
+      {items.map((project, index) => (
+        <Project key={index} project={project} />
       ))}
     </Container>
   );
@@ -41,9 +47,18 @@ const Container = styled(Flexbox)`
 `;
 
 const Item = styled(Flexbox)`
-  padding: 4px;
   margin: 4px;
-  transition: 0.3s opacity ${easing.default};
+  border-radius: 5px;
+  box-shadow: 0px 5px 10px -5px rgba(0, 0, 0, 0.3);
+  transition: box-shadow 0.5s;
+  will-change: transform;
+  border: 5px solid white;
+
+  transition: 0.15s opacity ${easing.default};
+
+  img {
+    border-radius: 5px;
+  }
 `;
 
 export { Projects };
